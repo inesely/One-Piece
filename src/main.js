a728a7c1da7e693ae968060399e15401ac4b39b5
inesely <ines.el.yanaoui@student.ehb.be>
2026-04-20 11:19:06 +0200

    Added One Piece API to project

---
+++ src/main.js
@@ -0,0 +1,28 @@
+
+const app = document.querySelector('#app');
+
+async function getCharacters() {
+  try {
+    const response = await fetch('https://api.jikan.moe/v4/anime?q=one%20piece');
+    const data = await response.json();
+
+    const characters = data.data;
+
+    app.innerHTML = `
+      <h1>One Piece</h1>
+      <div>
+        ${characters.slice(0, 10).map(char => `
+          <div style="margin-bottom:20px;">
+            <h2>${char.title}</h2>
+            <img src="${char.images.jpg.image_url}" width="200"/>
+          </div>
+        `).join('')}
+      </div>
+    `;
+  } catch (error) {
+    console.error(error);
+    app.innerHTML = `<p>Erreur API</p>`;
+  }
+}
+
+getCharacters();
