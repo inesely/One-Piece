92c6e2fe10a08c4902384a62429e9f6adb6f03e5
inesely <ines.el.yanaoui@student.ehb.be>
2026-04-28 09:45:30 +0200

    probleem van search bar opgelost

--- src/main.js
+++ src/main.js
@@ -1,28 +1,57 @@
-
 const app = document.querySelector('#app');
+
+let allCharacters = [];
+
+app.innerHTML = `<p>Loading...</p>`;
 
 async function getCharacters() {
   try {
     const response = await fetch('https://api.jikan.moe/v4/anime?q=one%20piece');
     const data = await response.json();
 
-    const characters = data.data;
+    allCharacters = data.data.slice(0, 20);
 
     app.innerHTML = `
       <h1>One Piece</h1>
-      <div class="characters">
-        ${characters.slice(0, 10).map(char => `
-          <div class="card">
-            <h2>${char.title}</h2>
-            <img src="${char.images.jpg.image_url}" width="200"/>
-          </div>
-        `).join('')}
-      </div>
+
+      <input 
+        type="text" 
+        id="searchInput" 
+        placeholder="Search a One Piece title..." 
+      />
+
+      <div class="characters" id="charactersContainer"></div>
     `;
+
+    displayCharacters(allCharacters);
+
+    const searchInput = document.querySelector('#searchInput');
+
+    searchInput.addEventListener('input', function () {
+      const searchValue = searchInput.value.toLowerCase();
+
+      const filteredCharacters = allCharacters.filter(char =>
+        char.title.toLowerCase().includes(searchValue)
+      );
+
+      displayCharacters(filteredCharacters);
+    });
+
   } catch (error) {
     console.error(error);
     app.innerHTML = `<p>Erreur API</p>`;
   }
+}
+
+function displayCharacters(characters) {
+  const charactersContainer = document.querySelector('#charactersContainer');
+
+  charactersContainer.innerHTML = characters.map(char => `
+    <div class="card">
+      <h2>${char.title}</h2>
+      <img src="${char.images.jpg.image_url}" alt="${char.title}" />
+    </div>
+  `).join('');
 }
 
 getCharacters();
